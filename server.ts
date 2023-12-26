a24cc0c1f60dfddb787ecc8d92e737265ff92c05
Nicolás Gonzálvez <n.gonzalvez@outlook.com>
2023-12-26 20:14:06 -0300

    Update styles

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { v4 as uuidv4 } from "uuid";
 
 //---------------------------------------------------------------------------------------------------------------------
 //                                                   HTTP SERVER
@@ -13,7 +14,11 @@ app.set("view engine", "ejs");
 // Serve the static files in the public folder.
 app.use(express.static("public"));
 
-// Define the routes here...
+// When accessing the root path, redirect the user to a new room.
+app.get("/", (_, res) => res.redirect(`/room/${uuidv4()}`));
+
+// When accessing a room, render the room.ejs file.
+app.get("/room/:roomId", (req, res) => res.render("room", { roomId: req.params.roomId }));
 
 //---------------------------------------------------------------------------------------------------------------------
 //                                             WEB-SOCKET RElAY SERVER
