efb114ababa78fae670a83a3db40c5e364bb4e03
Nicolás Gonzálvez <n.gonzalvez@outlook.com>
2023-12-27 13:50:09 -0300

    Add base front-end code

---
+++ public/main.js
@@ -0,0 +1,31 @@
+//----------------------------------------------------------------------------------------------------------------------
+//                                                  GENERAL
+//----------------------------------------------------------------------------------------------------------------------
+async function init() {}
+
+window.onload = init;
+
+//----------------------------------------------------------------------------------------------------------------------
+//                                                UI UTILITIES
+//----------------------------------------------------------------------------------------------------------------------
+function addParticipantStream(stream, peerId) {
+  // Create the video preview.
+  const video = document.createElement("video");
+  video.srcObject = stream;
+  video.autoplay = true;
+  video.onclick = () => setActiveParticipantStream(stream);
+  video.setAttribute("data-peer-id", peerId);
+
+  // Append it to the participants container.
+  const container = document.getElementById("participants");
+  container.appendChild(video);
+}
+
+function setActiveParticipantStream(stream) {
+  const currentVideo = document.getElementById("active-participant");
+  currentVideo.srcObject = stream;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+//                                             P2P CONNECTION LOGIC
+//----------------------------------------------------------------------------------------------------------------------
